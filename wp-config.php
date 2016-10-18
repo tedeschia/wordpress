5b3b2b999217753adde0efef3ff23eb56b8e2a42
tedeschia <atedeschi@entaconsulting.com>
2016-10-18 19:51:54 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -76,10 +76,10 @@ if ( !defined('ABSPATH') )
 require_once(ABSPATH . 'wp-settings.php');
 
 /* Multisitio */
-define( 'WP_ALLOW_MULTISITE', true );
+/*define( 'WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
 define('DOMAIN_CURRENT_SITE', 'entawebpublic.azurewebsites.net');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);*/
